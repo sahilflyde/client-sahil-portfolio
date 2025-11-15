6fed72aec02b9d7e7552cc81f810e0c93be11c34
Sahil Kalkal <sahil@flyde.in>
2025-11-15 12:26:09 +0530

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,10 @@
+
+import SectionHeader from "./sectionHeader";
+import ImageCard from "./card";
+import GridSection from "./GridWrapper";
+
+export const componentRegistry = {
+  "section-header": SectionHeader,
+  "image-card": ImageCard,
+  "grid-section": GridSection
+};
